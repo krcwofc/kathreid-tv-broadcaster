bc7d815b1e73d2b6f8ea409b0144bfc708f49311
krcwofc <krcwofficial@gmail.com>
2026-06-07 11:37:34 -0700

    Update discord.js

--- cron/discord.js
+++ cron/discord.js
@@ -1,1 +1,14 @@
+export async function sendDiscordMessage(content) {
+  if (!process.env.DISCORD_WEBHOOK_URL) {
+    console.error("Missing DISCORD_WEBHOOK_URL");
+    return;
+  }
 
+  await fetch(process.env.DISCORD_WEBHOOK_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ content })
+  });
+}
